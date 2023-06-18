aea11d1bfcc7e8f52789baf230d17bbbdb157d7b
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-06-18 13:30:07 +0900

    関数のパラメーター(オプション・デフォルト)と戻り値の型 終了

--- src/index.ts
+++ src/index.ts
@@ -7,9 +7,29 @@
 /**
  * ↓↓↓ 03.基本の型定義 ↓↓↓
  */
-import { unknownSample, anySample, notExistSample, primitiveSample } from './basic'
+// import { unknownSample, anySample, notExistSample, primitiveSample } from './basic'
 
-unknownSample()
-anySample()
-notExistSample()
-primitiveSample()
+// unknownSample()
+// anySample()
+// notExistSample()
+// primitiveSample()
+
+/**
+ * ↓↓↓ 04.関数の型定義 ↓↓↓
+ */
+import { logMessage, logMessage2, logMessage3, logMessage4, logMessage6, logMessage7 } from './function/basic'
+import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+
+logMessage('Hello TypeScript!')
+logMessage2('Hello TypeScript2!')
+logMessage3('Hello TypeScript3!')
+logMessage4('Hello TypeScript4!')
+
+isUserSignedIn('ABC', 'Torahack')
+isUserSignedIn('DEF', 'Torahack')
+isUserSignedIn2('ABC')
+const sum = sumProductsPrice(100, 200, 300, 400, 500)
+console.log('Function parameters sample 5:', sum)
+
+logMessage6('Hello 666!!!')
+logMessage7('Hello 777!!!')
