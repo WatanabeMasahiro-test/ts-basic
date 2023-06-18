e458081969d264b8e525b7d90415d7092fe57355
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-06-18 22:29:22 +0900

    ジェネリック型を使いながらポリモーフィズムを理解する

---
+++ src/generics/advanced.ts
@@ -0,0 +1,30 @@
+export default function genericsAdvancedSample() {
+  // map関数のシグネチャ
+  type Map<T, U> = (array: T[], fn: (item: T) => U) => U[]
+
+  const mapStringsToNumbers: Map<string, number> = (array, fn) => { // 文字列から数値型へ
+    const result = []
+    for (let i=0; i < array.length; i++) {
+      const item = array[i]
+      result[i] = fn(item)
+    }
+    return result
+  }
+
+  const numbers = mapStringsToNumbers(['123', '456', '789'], (item) => Number(item))
+  console.log('Generics advanced sample 1:', numbers)
+
+
+  const mapNumbersToStrings: Map<number, string> = (array, fn) => { // 数値型から文字列へ
+    const result = []
+    for (let i = 0; i < array.length; i++) {
+      const item = array[i]
+      result[i] = fn(item)
+    }
+    return result
+  }
+
+  // const strings = mapNumbersToStrings([123, 456, 789], (item) => String(item))
+  const strings = mapNumbersToStrings(numbers, (item) => String(item))
+  console.log('Generics advanced sample 1:', strings)
+}
