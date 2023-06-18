8a46e2772a6a877b431b5e36768a0e370cc8f033
WatanabeMasahiro <2c.nave.stk@gmail.com>
2023-06-18 13:53:50 +0900

    オブジェクトの型定義の基礎

--- src/index.ts
+++ src/index.ts
@@ -17,19 +17,26 @@
 /**
  * ↓↓↓ 04.関数の型定義 ↓↓↓
  */
-import { logMessage, logMessage2, logMessage3, logMessage4, logMessage6, logMessage7 } from './function/basic'
-import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
+// import { logMessage, logMessage2, logMessage3, logMessage4, logMessage6, logMessage7 } from './function/basic'
+// import { isUserSignedIn, isUserSignedIn2, sumProductsPrice } from './function/parameters'
 
-logMessage('Hello TypeScript!')
-logMessage2('Hello TypeScript2!')
-logMessage3('Hello TypeScript3!')
-logMessage4('Hello TypeScript4!')
+// logMessage('Hello TypeScript!')
+// logMessage2('Hello TypeScript2!')
+// logMessage3('Hello TypeScript3!')
+// logMessage4('Hello TypeScript4!')
 
-isUserSignedIn('ABC', 'Torahack')
-isUserSignedIn('DEF', 'Torahack')
-isUserSignedIn2('ABC')
-const sum = sumProductsPrice(100, 200, 300, 400, 500)
-console.log('Function parameters sample 5:', sum)
+// isUserSignedIn('ABC', 'Torahack')
+// isUserSignedIn('DEF', 'Torahack')
+// isUserSignedIn2('ABC')
+// const sum = sumProductsPrice(100, 200, 300, 400, 500)
+// console.log('Function parameters sample 5:', sum)
 
-logMessage6('Hello 666!!!')
-logMessage7('Hello 777!!!')
+// logMessage6('Hello 666!!!')
+// logMessage7('Hello 777!!!')
+
+/**
+ * ↓↓↓ 05.オブジェクトの型定義 ↓↓↓
+ */
+import objectSample from './object/object'
+
+objectSample()
---
+++ src/object/object.ts
@@ -0,0 +1,27 @@
+export default function objectSample() {
+  // const a: object = {
+  //   name: 'Torahack',
+  //   age: 28
+  // }
+  // a.name   nameが型objectに存在しないと言われる
+
+  /**
+   * オブジェクトリテラル記法で型定義
+   */
+  let country: {
+    language: string
+    name: string
+  } = {
+    language: 'Japanese',
+    name: 'Japan',
+  }
+
+  console.log('Object object sample 1:', country)
+
+  country = {
+    language: 'English',
+    name: 'United States of America',
+  }
+
+  console.log('Object object sample 2:', country)
+}
